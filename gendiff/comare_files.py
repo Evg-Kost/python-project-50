d5e2dfdadcec2a5a68952154eb52c89cdc9e52e2
Evgeny Kostolomov <e.kostolomov@bk.ru>
2025-03-27 18:04:59 +0300

    fix lint

--- gendiff/comare_files.py
+++ gendiff/comare_files.py
@@ -10,27 +10,11 @@ def compare_files(data1, data2):
         elif key not in data2:
             result[key] = {'status': 'deleted', 'value': data1[key]}
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            result[key] = {'status': 'nested', 'value': compare_files(data1[key], data2[key])}
+            result[key] = {'status': 'nested', 'value':
+                compare_files(data1[key], data2[key])}
         elif data1[key] != data2[key]:
-            result[key] = {'status': 'update', 'old_value': data1[key], 'new_value': data2[key]}
+            result[key] = {'status': 'update', 'old_value':
+                data1[key], 'new_value': data2[key]}
         elif data1[key] == data2[key]:
             result[key] = {'status': 'no_change', 'value': data1[key]}
     return result
-
-# filepath1 = {
-#   "host": "hexlet.io",
-#   "timeout": 50,
-#   "proxy": "123.234.53.22",
-#   "follow": False
-# }
-# filepath2 = {
-#     "timeout": 20,
-#     "verbose": True,
-#     "host": "hexlet.io"
-# }
-# filepath1 = json.load(open('/home/evgeny/PycharmProjects/PythonProject/python-project-50/tests/test_data/file1_7.json'))
-# filepath2 = json.load(open('/home/evgeny/PycharmProjects/PythonProject/python-project-50/tests/test_data/file2_7.json'))
-#
-# print(compare_files(filepath1, filepath2))
-# print()
-# print(stylish(compare_files(filepath1, filepath2)))
--- gendiff/formats/stylish.py
+++ gendiff/formats/stylish.py
@@ -12,7 +12,8 @@ def check_type(values, level):
     predict = level * '    '
     if isinstance(values, dict):
         for key in values:
-            result.append(f'{predict}{key}: {check_type(values[key], level + 1)}')
+            result.append(f'{predict}{key}: '
+                          f'{check_type(values[key], level + 1)}')
     else:
         return normalize(values)
     return '{\n' + '\n'.join(result) + '\n' + predict[:-4] + '}'
@@ -24,16 +25,22 @@ def stylish(data, level=1):
     for key, value in data.items():
         match value.get('status', ):
             case 'added':
-                items.append(f'{predict[:-2]}+ {key}: {check_type(value["value"], level + 1)}')
+                items.append(f'{predict[:-2]}+ {key}: '
+                             f'{check_type(value["value"], level + 1)}')
             case 'deleted':
-                items.append(f'{predict[:-2]}- {key}: {check_type(value["value"], level + 1)}')
+                items.append(f'{predict[:-2]}- {key}: '
+                             f'{check_type(value["value"], level + 1)}')
             case 'update':
-                items.append(f'{predict[:-2]}- {key}: {check_type(value["old_value"], level + 1)}')
-                items.append(f'{predict[:-2]}+ {key}: {check_type(value["new_value"], level + 1)}')
+                items.append(f'{predict[:-2]}- {key}: '
+                             f'{check_type(value["old_value"], level + 1)}')
+                items.append(f'{predict[:-2]}+ {key}: '
+                             f'{check_type(value["new_value"], level + 1)}')
             case 'no_change':
-                items.append(f'{predict[:-2]}  {key}: {check_type(value["value"], level + 1)}')
+                items.append(f'{predict[:-2]}  {key}: '
+                             f'{check_type(value["value"], level + 1)}')
             case 'nested':
-                items.append(f'{predict[:-2]}  {key}: {stylish(value["value"], level + 1)}')
+                items.append(f'{predict[:-2]}  {key}: '
+                             f'{stylish(value["value"], level + 1)}')
             case _:
                 print('error', value)
     return '{\n' + '\n'.join(items) + '\n' + predict[:-4] + '}'
