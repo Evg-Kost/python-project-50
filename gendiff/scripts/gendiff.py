2fca2ad6e2137aceb0c6c84efbe4c9e153b7fec8
Evgeny Kostolomov <e.kostolomov@bk.ru>
2025-04-02 05:36:16 +0300

    fix bug

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -21,9 +21,9 @@ def main():
         description="Compares two configuration files and shows a difference.")
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
-    parser.add_argument("-f", "--format", type=str, help="set format of output", default="stylish")
+    parser.add_argument("-f", "--format", type=str,
+                        help="set format of output", default="stylish")
     args = parser.parse_args()
-    print(args)
     print(gendiff(args.first_file, args.second_file, args.format))
 
 
